099fbee97abb9dca00162e942f2746df6fd8ea58
Patryk <krasowski.p@outlook.com>
2023-11-20 19:57:22 +0100

    created timerExpired state. Add conditional display paragraf depending on state. Created handleStart function which setTimeout

--- src/components/TimerChallange.jsx
+++ src/components/TimerChallange.jsx
@@ -1,12 +1,21 @@
+import { useState } from 'react'
+
 const TimerChallange = ({ title, targetTime }) => {
+	const [timerExpired, setTimerExpired] = useState(false)
+	const handleStart = () => {
+		setTimeout(() => {
+			setTimerExpired(true)
+		}, targetTime * 1000)
+	}
 	return (
 		<section className='challenge'>
 			<h2>{title}</h2>
+			{timerExpired && <p>You lost!</p>}
 			<p className='challenge-time'>
 				{targetTime} second{targetTime > 1 ? 's' : ''}
 			</p>
 			<p>
-				<button>Start Challange</button>
+				<button onClick={handleStart}>Start Challange</button>
 			</p>
 			<p className=''>Time is running... / Timer inactive</p>
 		</section>
