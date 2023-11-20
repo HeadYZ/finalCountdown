2dc1ee454ce7974fbf64fbff17124398ff625456
Patryk <krasowski.p@outlook.com>
2023-11-20 19:29:07 +0100

    created userInput Ref, connect with input, created handleClick function assigned to button

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,21 @@
+import { useRef, useState } from 'react'
+
 export default function Player() {
-  return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
-      <p>
-        <input type="text" />
-        <button>Set Name</button>
-      </p>
-    </section>
-  );
+	const [playerName, setPlaterName] = useState(null)
+
+	const userInput = useRef()
+
+	const handleClick = () => {
+		setPlaterName(userInput.current.value)
+	}
+
+	return (
+		<section id='player'>
+			<h2>Welcome {playerName ? playerName : 'unknown entity'}</h2>
+			<p>
+				<input type='text' ref={userInput} />
+				<button onClick={handleClick}>Set Name</button>
+			</p>
+		</section>
+	)
 }
